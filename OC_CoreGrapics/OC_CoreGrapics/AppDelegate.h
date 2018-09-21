8f0d4a50b6647659d0d9ca4c0222d5564674939e
wenze <wenze@wenzedeAir.workgroup>
2018-09-21 16:34:44 +0800

    iOS绘图系统CoreGraphics

---
+++ OC_CoreGrapics/OC_CoreGrapics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC_CoreGrapics
+//
+//  Created by wenze on 2018/9/21.
+//  Copyright © 2018年 wenze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC_CoreGrapics/OC_CoreGrapics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC_CoreGrapics
+//
+//  Created by wenze on 2018/9/21.
+//  Copyright © 2018年 wenze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
