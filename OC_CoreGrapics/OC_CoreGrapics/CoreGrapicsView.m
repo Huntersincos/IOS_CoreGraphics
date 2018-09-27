05714f4ff26ea5887f7311a5679ac42759423d30
wenze <wenze@wenzedeMacBook-Air.local>
2018-09-27 10:02:55 +0800

    绘制椭圆和圆

--- OC_CoreGrapics/OC_CoreGrapics/CoreGrapicsView.m
+++ OC_CoreGrapics/OC_CoreGrapics/CoreGrapicsView.m
@@ -13,7 +13,9 @@
  -(void)drawRect:(CGRect)rect
 {
     [super drawRect:rect];
-    [self drawOneRectangle:rect];
+    //[self drawOneRectangle:rect];
+   // [self drawCircleAtx:rect];
+    [self drawEllipse:rect];
 }
 
 /**
@@ -33,6 +35,39 @@
     CGContextFillPath(ctx);
 }
 
+/**
+ 绘制一个圆
+ CGContextAddArc
+ x:圆弧的中心点坐标X
+ y:曲线控制点的y坐标
+ radius:指定的x坐标值
+ startAngle:弧的起点与x轴的夹角
+ endAngle:弧的终点与x轴的夹角
+ clockWise:指定值1
+ **/
+
+- (void)drawCircleAtx:(CGRect)rect
+{
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextAddArc(ctx, rect.origin.x + rect.size.width/2, rect.origin.y + rect.size.width/2, rect.size.width/2, 0, 2* M_PI, 1);
+    CGContextSetFillColorWithColor(ctx, [UIColor orangeColor].CGColor);
+    CGContextFillPath(ctx);
+
+}
+
+/**
+ 绘制一个椭圆
+ **/
+
+-(void)drawEllipse:(CGRect)rect
+{
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextAddEllipseInRect(ctx, rect);
+    CGContextSetFillColorWithColor(ctx, [UIColor redColor].CGColor);
+    CGContextFillPath(ctx);
+    CGContextStrokePath(ctx);
+    
+}
 
 /*
 // Only override drawRect: if you perform custom drawing.
